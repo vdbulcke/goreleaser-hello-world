9a1c2c94fd2a304fe4d7498045f00071da0c11ba
vdbulcke <cedric.vandenbulcke.linux@gmail.com>
2021-11-14 15:10:30 +0100

    test scheduler

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module hello-world
 
 go 1.16
+
+require (
+	github.com/carlescere/scheduler v0.0.0-20170109141437-ee74d2f83d82 // indirect
+	github.com/procyon-projects/chrono v1.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/procyon-projects/chrono"
+)
 
 // GitCommit the current git commit
 // will be injected during build
@@ -15,5 +22,28 @@ var HumanVersion = fmt.Sprintf("%s-(%s)", Version, GitCommit)
 func main() {
 
 	fmt.Printf("Hello world %s\n", HumanVersion)
+	// job := func() {
+	// 	t := time.Now()
+	// 	fmt.Println("Time's up! @", t.UTC())
+	// }
+
+	// onetimejob := func() {
+	// 	t := time.Now()
+	// 	fmt.Println("One time! @", t.UTC())
+	// }
+	now := time.Now()
+	taskScheduler := chrono.NewDefaultTaskScheduler()
+
+	_, err := taskScheduler.Schedule(func(ctx context.Context) {
+		log.Print("One-Shot Task")
+	}, chrono.WithStartTime(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second()+1))
+
+	if err == nil {
+		log.Print("Task has been scheduled successfully.")
+	}
+
+	time.Sleep(30 * time.Second)
+	// // Keep the program from not exiting.
+	// runtime.Goexit()
 
 }
